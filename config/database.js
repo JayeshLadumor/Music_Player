2492d95e8ad2c7f58fb9f9d6f754bc5e6ab36192
Jayesh Ladumor <jayesh.p.ladumor@gmail.com>
2017-11-29 11:17:09 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 module.exports = {
- database: 'mongodb://localhost:27017/musicstreamingdb',
-     /* database: 'mongodb://jayeshladumor:jayesh123@ds111123.mlab.com:11123/onlinemusicstreaming',*/
-    secret:'yoursecret'
+     database: 'mongodb://localhost:27017/musicstreamingdb',
+     secret:'yoursecret'
 }
